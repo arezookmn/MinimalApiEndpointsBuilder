04f7328fdc2f00dcf4e3c9ed5c48e05f035671a8
arezookmn <arezookmn1@gmail.com>
2024-03-20 13:40:10 +0330

    initial project setup

---
+++ MinimalApiApplication/Program.cs
@@ -0,0 +1,21 @@
+using MinimalApiApplication.EndpointBuilder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.MapEntitiesEndpoint();
+
+app.Run();
+
+
